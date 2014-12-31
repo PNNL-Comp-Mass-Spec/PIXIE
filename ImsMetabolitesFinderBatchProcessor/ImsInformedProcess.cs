174950e71834f6e7046a624bd655f19071c22dd0
Jian <jian.ma@pnnl.gov>
2014-12-31 19:03:39 +0000

    Added the missing file I forgot the version.
    git-svn-id: http://prismsvn.pnl.gov/svn/User_Applications/Monroe_Utilities/IMSMzFinder@19100 91f24f89-0e13-0410-88a5-a0bb1538c76e

---
+++ ImsMetabolitesFinderBatchProcessor/ImsInformedProcess.cs
@@ -0,0 +1,43 @@
+﻿namespace ImsMetabolitesFinderBatchProcessor
+{
+    using System.Collections.Generic;
+    using System.Diagnostics;
+
+    [System.ComponentModel.DesignerCategory("Code")]
+    public class ImsInfomredProcess : Process
+    {
+        public int JobID { get; private set; }
+        public string DataSetName { get; set; }
+        public bool Done { get; set; }
+        public HashSet<string> FileResources { get; set; }
+
+
+        public ImsInfomredProcess(int ID, string name, string exe, string arguments, bool shell)
+        {
+            this.JobID = ID;
+            this.DataSetName = name;
+            this.StartInfo.FileName = exe;
+            this.StartInfo.Arguments = arguments;
+            this.StartInfo.UseShellExecute = shell;
+            this.StartInfo.CreateNoWindow = !shell;
+            this.FileResources = new HashSet<string>();
+            this.Done = false;
+        }
+
+        public bool AreResourcesFree(IEnumerable<ImsInfomredProcess> tasks)
+        {
+            bool free = true;
+            foreach (var task in tasks)
+            {
+                HashSet<string> sharedResources = new HashSet<string>(this.FileResources);
+                sharedResources.IntersectWith(task.FileResources);
+                if (sharedResources.Count != 0)
+                {
+                    free = false;
+                    break;
+                }
+            }
+            return free;
+        }
+    }
+}
